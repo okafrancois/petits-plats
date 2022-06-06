a279df33757700559df2693095e8731e348e7bbe
Benry <itoutouberny@gmail.com>
2022-06-06 20:31:09 +0200

    feat(various): add search function and result update

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -6,20 +6,7 @@
 
 
 function getSearchResults(searchTerm, data) {
-    const results = [];
-
-    data.forEach(recipe => {
-        const recipeName = recipe.name.toLowerCase();
-        const nameTest = recipeName.includes(searchTerm);
-        const ingredientsTest = recipe.ingredients.some(item => item.ingredient.toLowerCase().includes(searchTerm));
-        const descriptionTest = recipe.description.toLowerCase().includes(searchTerm);
-
-        if (nameTest || ingredientsTest || descriptionTest) {
-            results.push(recipe);
-        }
-    });
-
-    return results;
+    return data.filter(recipe => recipe.name.toLowerCase().includes(searchTerm) || recipe.ingredients.some(item => item.ingredient.toLowerCase().includes(searchTerm)) || recipe.description.toLowerCase().includes(searchTerm));
 }
 
 
