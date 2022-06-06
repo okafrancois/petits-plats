17a40c2750cbbf0c4aa28830b5b2824d8e3a9327
Benry <itoutouberny@gmail.com>
2022-06-06 20:21:27 +0200

    feat(various): add search function and result update

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 import {recipes} from "../data/recipes.js";
 import BasicRecipe from "./models/BasicRecipe.js";
 import RecipeCard from "./templates/RecipeCard.js";
-import {initSearch} from "./utils/search.js";
+import {getSearchResults} from "./utils/search.js";
 
 /*
   Index
@@ -13,6 +13,8 @@ class App {
     constructor(data) {
         
         this.recipes = data;
+        this.currentSearchTerm = "";
+        this.currentSearchResults = [...this.recipes];
     }
 
     get recipesContainer() {
@@ -20,6 +22,8 @@ class App {
     }
 
     renderRecipes(data) {
+        this.recipesContainer.innerHTML = "";
+
         data = data || this.recipes;
         data
             .map(recipe => new BasicRecipe(recipe))
@@ -29,9 +33,23 @@ class App {
             });
     }
 
+    updateSearchResults(searchTerm) {
+        this.currentSearchTerm = searchTerm;
+        this.currentSearchResults = getSearchResults(searchTerm, this.recipes);
+
+        this.renderRecipes(this.currentSearchResults);
+    }
+
     init() {
         const searchInput = document.querySelector(".filters .search__bar");
+
         this.renderRecipes(this.recipes);
+
+        searchInput.addEventListener("change", (e) => {
+            const searchTerm = e.target.value.toLowerCase();
+            this.updateSearchResults(searchTerm);
+        })
+
     }
 }
 
--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -3,3 +3,24 @@
   ---------- ---------- ---------- ---------- ----------
   • Base
 */
+
+
+function getSearchResults(searchTerm, data) {
+    const results = [];
+
+    data.forEach(recipe => {
+        const recipeName = recipe.name.toLowerCase();
+        const nameTest = recipeName.includes(searchTerm);
+        const ingredientsTest = recipe.ingredients.some(item => item.ingredient.toLowerCase().includes(searchTerm));
+        const descriptionTest = recipe.description.toLowerCase().includes(searchTerm);
+
+        if (nameTest || ingredientsTest || descriptionTest) {
+            results.push(recipe);
+        }
+    });
+
+    return results;
+}
+
+
+export { getSearchResults };
