389383679487c8e405ac125f2c3191f9558fa215
Benry <itoutouberny@gmail.com>
2022-06-23 15:58:44 +0200

    refactor(various): remove cover image

--- scripts/templates/recipe-card.js
+++ scripts/templates/recipe-card.js
@@ -4,7 +4,7 @@ const RecipeCard = (recipe) => (
   `<li class="recipes__item recipe-card" data-id="${recipe.id}">
                 <div class="recipe-card__container">
                     <div class="recipe-card__cover">
-                        <img src="https://picsum.photos/800/400?random=${getRandomInt(1, 1000)}" alt="${recipe.title}">
+                     
                     </div>
                     <div class="recipe-card__content">
                         <h3 class="recipe-card__title">${recipe.name}</h3>
